5b407d1ab0f8060ee8ac48cbf040983db4899658
MoatezDh <74154294+MoatezDh@users.noreply.github.com>
2024-10-27 10:47:01 +0100

    Update appfinal.py

--- appfinal.py
+++ appfinal.py
@@ -16,13 +16,11 @@ CORS(app)  # Allow all origins by default
 reference_image_path = os.path.join(os.path.dirname(__file__), 'chawki.jpg')
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/facerecognition', methods=['POST'])  # Define the route for face recognition
 def face_recognition():
     # Check if an image is provided in the request
     if 'image' not in request.files:
-        logging.warning("No image provided in the request.")
         return jsonify({"message": "No image provided"}), 400
     
     image_file = request.files['image']
@@ -33,7 +31,6 @@ def face_recognition():
         frame = cv2.imdecode(in_memory_file, cv2.IMREAD_COLOR)
 
         if frame is None:
-            logging.error("Failed to decode image.")
             return jsonify({"message": "Failed to decode image"}), 400
 
         # Verify the received image against the reference image
@@ -42,26 +39,20 @@ def face_recognition():
         # Check if the person is verified
         if result['verified']:
             response_message = "This is YOU!"
-            logging.info("User verified as the reference person.")
         else:
             # Save the current frame if the user is not verified
             timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             filename = f'intrus_{timestamp}.jpg'
             cv2.imwrite(filename, frame)  # Save the image
             response_message = "Not You!"
-            logging.info(f"User NOT verified. Intruder image saved as '{filename}'.")
 
     except Exception as e:
-        logging.error(f"Face recognition failed: {e}")
-        logging.error(traceback.format_exc())  # Log the full traceback
         return jsonify({"message": "Face recognition failed"}), 500
 
     return jsonify({"face_recognition_result": result['verified'], "message": response_message}), 200
 
 # Error handler for unhandled routes
-@app.errorhandler(404)
-def not_found(e):
-    return jsonify({"message": "Resource not found"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)  # Run the app in debug mode for development
